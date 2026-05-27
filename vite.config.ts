2a419cd739907cd31e4fd409cf39ab359da84775
Ivan Stepanov <ivsestepanov@edu.hse.ru>
2026-05-27 23:50:03 +0300

    feat: configure deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/graph-algorithms/",
   plugins: [react(), VitePWA()],
   server: {
     host: "0.0.0.0",
