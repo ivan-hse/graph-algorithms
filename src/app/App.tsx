baffbe300c3957368506cb52006d61a71fb289d7
Ivan Stepanov <ivsestepanov@edu.hse.ru>
2026-05-28 00:09:58 +0300

    fix: router base path

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,10 +15,12 @@ import { ROUTES } from "@/shared/static";
 
 import { theme } from "./theme.ts";
 
+const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 const App = () => {
   return (
     <MantineProvider forceColorScheme="dark" theme={theme}>
-      <Router>
+      <Router base={base}>
         <Switch>
           <Route path={ROUTES.START} component={StartPage} />
           <Route path={ROUTES.BFS} component={BFSPage} />
